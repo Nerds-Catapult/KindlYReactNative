591191b3f1f48be3e0635de632db3f80f74b425a
shadmeoli <shadrack@shopzetu.com>
2024-03-14 16:54:06 +0300

    update: full app setup [home page skeleton and welcome screen]

---
+++ App.tsx
@@ -0,0 +1,61 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { SafeAreaView, StyleSheet } from "react-native";
+import { Asset } from "expo-asset";
+import * as FileSystem from "expo-file-system";
+
+
+import WelcomeScreen from "./screens/Auth/WelcomeScreen";
+import LoginScreen from "./screens/Auth/LoginScreen";
+import SignupScreen from "./screens/Auth/SignupScreen";
+import HomeScreen from "./screens/Home/HomeScreen";
+import { Auth } from "./utils/access_authentication";
+
+const app = new Auth()
+const Stack = createStackNavigator();
+
+export default function App() {
+
+  return (
+    <SafeAreaView className="flex-1">
+      <StatusBar
+        animated={true}
+        backgroundColor="#61dafb"
+      />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Welcome">
+          <Stack.Screen
+            name="Welcome"
+            component={WelcomeScreen}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="login"
+            component={LoginScreen}
+            options={{
+              headerShown: true,
+            }}
+          />
+          <Stack.Screen
+            name="signup"
+            component={SignupScreen}
+            options={{
+              headerShown: true,
+            }}
+          />
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
+  );
+}
---
+++ interfaces/Auth.ts
@@ -0,0 +1,49 @@
+export interface User {
+  email: string | undefined;
+  password: string | undefined;
+}
+
+export interface SignupCredentials {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  "Phone number": string;
+}
+
+export interface ErrorLoginResponse {
+  errCode: string;
+  resCode: string;
+  resMsg: string;
+}
+
+export interface Result {
+  identity: string;
+  redirectUrl: string
+}
+
+export interface LoginResponse {
+  resCode: string;
+  resMsg: string;
+  result: Result
+}
+
+export interface AuthHeaders {
+  grant_type: string;
+  client_id: string;
+  client_secret: string;
+}
+
+export interface AuthResponse {
+  access_token: string;
+  expires_in: number; // in seconds
+  token_type: string; // token type should be Bearer
+}
+
+
+export interface SignupResponse {
+  resCode: string;
+  resMsg: string;
+  result: Result
+}
+
